ee80e3d7751cba5076e9e761a3cfd3900a8d8e2d
bnd <zename9@gmail.com>
2024-10-30 18:53:47 +0400

    b: add premission

--- handyman/forms.py
+++ handyman/forms.py
@@ -1,6 +1,6 @@
 # forms.py
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -10,3 +10,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        if commit:
+            # Назначаем группу "Обычные пользователи"
+            normal_user_group = Group.objects.get(name='user')
+            user.groups.add(normal_user_group)
+        return user
