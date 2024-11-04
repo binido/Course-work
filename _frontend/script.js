a44957182bcb07fe108aa8d6c4325241e741ff2b
bnd <zename9@gmail.com>
2024-11-04 18:56:21 +0400

    f: adaptive master page

--- _frontend/script.js
+++ _frontend/script.js
@@ -1,3 +1,11 @@
 function toggleMobileMenu(menu) {
-  menu.classList.toggle("open");
+  menu.classList.toggle("open"); // Переключаем класс .open у #hamburger-icon
+
+  if (menu.classList.contains("open")) {
+    document.body.style.height = "100vh"; // Устанавливаем высоту body на 100vh
+    document.body.style.overflow = "hidden"; // Отключаем прокрутку страницы
+  } else {
+    document.body.style.height = ""; // Возвращаем высоту body в исходное состояние
+    document.body.style.overflow = ""; // Включаем прокрутку
+  }
 }
